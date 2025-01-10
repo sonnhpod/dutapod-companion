2978e162bd70516b16b6c524167d1fb7ffc00fdf
sonnhpod <sonnhpod@gmail.com>
2025-01-10 10:42:39 +0700

    add content for footer shortcode
    Add additional content for the footer shortcode

--- includes/template/scope-frontend/page/front-page-footer.php
+++ includes/template/scope-frontend/page/front-page-footer.php
@@ -1,104 +1,71 @@
-<div id="dutapod-footer-container" class="dutapod-footer-container">
-    <div class="footer__top page-width">
-        <div class="footer__top-wrapper f-flex">
-            <div class="footer-block footer-block--contact_information f-column w-full" style="--col-width:33%;">
-                <motion-element class="block" data-motion="fade-up" data-motion-delay="50">
-                        
-                    <details id="Details-contact_information_gxjDVM-sections--17885087957157__footer" is="footer-details" open="" data-open-default="true" class="accordion-details footer-accordion-details" aria-expanded="true" aria-controls="">
-                        <summary class="accordion-details__summary flex items-center justify-between gap-2">
-                            <h3 class="footer-block__heading inline-richtext h4">Contact Us</h3>
-                            <span class="flex items-center lg:hidden">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-plus-toggle icon--medium" viewBox="0 0 20 20" fill="none">
-                                    <path class="horizontal" d="M3.125 10H16.875" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                    <path class="vertical" d="M10 3.125L10 16.875" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                </svg>
-                            </span>
-                        </summary>
-                        <div class="accordion-details__content footer-block__contact-info footer-info">                                
-                            <div class="footer-info__item footer-info__address">
-                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M10 10.625C11.3807 10.625 12.5 9.50571 12.5 8.125C12.5 6.74429 11.3807 5.625 10 5.625C8.61929 5.625 7.5 6.74429 7.5 8.125C7.5 9.50571 8.61929 10.625 10 10.625Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                                    <path d="M16.25 8.125C16.25 13.75 10 18.125 10 18.125C10 18.125 3.75 13.75 3.75 8.125C3.75 6.4674 4.40848 4.87769 5.58058 3.70558C6.75269 2.53348 8.3424 1.875 10 1.875C11.6576 1.875 13.2473 2.53348 14.4194 3.70558C15.5915 4.87769 16.25 6.4674 16.25 8.125Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                                </svg>
-                                <span>US Office:&nbsp;1942 Broadway #314cBoulder, CO 80302, USA</span>
-                            </div>                                    
-                                        
-                            <div class="footer-info__item footer-info__email">
-                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M2.5 4.375H17.5V15C17.5 15.1658 17.4342 15.3247 17.3169 15.4419C17.1997 15.5592 17.0408 15.625 16.875 15.625H3.125C2.95924 15.625 2.80027 15.5592 2.68306 15.4419C2.56585 15.3247 2.5 15.1658 2.5 15V4.375Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                                    <path d="M17.5 4.375L10 11.25L2.5 4.375" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                                </svg>
-                                <a href="mailto:cs@64hydro.com">cs@64hydro.com</a>
-                            </div>                                   
-                                        
-                            <div class="footer-info__item footer-info__working-hours">
-                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M10 17.5C14.1421 17.5 17.5 14.1421 17.5 10C17.5 5.85786 14.1421 2.5 10 2.5C5.85786 2.5 2.5 5.85786 2.5 10C2.5 14.1421 5.85786 17.5 10 17.5Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                                <path d="M10 5.625V10H14.375" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                                </svg>
-                                <span>Mon - Fri (9:00 - 18:00 - GMT+7)</span>
-                            </div>                                
-                        </div><!--.accordion-details__content.footer-block__contact-info.footer-info -->
-                    </details><!--#Details-contact_information_gxjDVM-sections--17885087957157__footer -->
-                        
-                </motion-element>
-            </div><!--.footer-block--contact_information -->
 
-            <div class="footer-block footer-block--menu f-column w-full" style="--col-width:33%;">
-                <motion-element class="block" data-motion="fade-up" data-motion-delay="100">
-                    <details id="Details-menu_DEKGYP-sections--17885087957157__footer" is="footer-details" open="" data-open-default="false" class="accordion-details footer-accordion-details" aria-expanded="true" aria-controls="">
-                        <summary class="accordion-details__summary flex items-center justify-between gap-2">
-                            <h3 class="footer-block__heading inline-richtext h4">Our Company</h3>
-                            <span class="flex items-center lg:hidden">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-plus-toggle icon--medium" viewBox="0 0 20 20" fill="none">
-                                    <path class="horizontal" d="M3.125 10H16.875" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                    <path class="vertical" d="M10 3.125L10 16.875" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                </svg>
-                            </span>
-                        </summary><ul class="accordion-details__content linklist linklist--vertical list-unstyled"><li>
-                        <a href="/pages/faqs" class="reversed-link">
-                        FAQs
-                        </a>
-                        </li><li>
-                            <a href="/pages/about-us" class="reversed-link">
-                            About Us
-                            </a>
-                        </li></ul>
-                    </details>
-                </motion-element>            
-            </div><!--.footer-block--menu -->
-            
-            <div class="footer-block footer-block--menu f-column w-full" style="--col-width:33%;">
-                <motion-element class="block" data-motion="fade-up" data-motion-delay="150">
-                    <details id="Details-menu_4eCrBB-sections--17885087957157__footer" is="footer-details" open="" data-open-default="false" class="accordion-details footer-accordion-details" aria-expanded="true" aria-controls="">
-                        <summary class="accordion-details__summary flex items-center justify-between gap-2">
-                            <h3 class="footer-block__heading inline-richtext h4">Customer Support</h3>
-                            <span class="flex items-center lg:hidden">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-plus-toggle icon--medium" viewBox="0 0 20 20" fill="none">
-                                    <path class="horizontal" d="M3.125 10H16.875" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                    <path class="vertical" d="M10 3.125L10 16.875" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                </svg>
-                            </span>
-                        </summary>
-                        <ul class="accordion-details__content linklist linklist--vertical list-unstyled">
-                            <li>
-                                <a href="/pages/contact-us" class="reversed-link">Contact Us</a>                           
-                            </li>
-                            <li>
-                                <a href="/pages/refund-policy" class="reversed-link">Refund Policy</a>
-                            </li>
-                            <li>
-                                <a href="/pages/shipping-delivery" class="reversed-link">Shipping &amp; Delivery</a>
-                            </li>
-                            <li>
-                                <a href="https://64hydro.com/apps/trackingmore" class="reversed-link">Order Tracking</a>
-                            </li>
-                            <li>
-                                <a href="/policies/privacy-policy" class="reversed-link">Privacy Policy</a>                                   
-                            </li>
-                        </ul>
-                    </details>
-                </motion-element>
-            </div><!--.footer-block.footer-block--menu.f-column.w-full --></div>
-        </div><!--.footer__top-wrapper f-flex -->
-</div><!--#dutapod-footer-container -->
+<div class="dutapod-footer-outer-container">
+    <div id="dutapod-footer-container" class="dutapod-footer-container">
+        <div class="row inner-footer-container">
+            <!--1. Contact us section - 1st column -->        
+            <div class="contact-us-container col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <h3 class="block_heading">Contact Us</h3>
+                <div class="info-item address">
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10 10.625C11.3807 10.625 12.5 9.50571 12.5 8.125C12.5 6.74429 11.3807 5.625 10 5.625C8.61929 5.625 7.5 6.74429 7.5 8.125C7.5 9.50571 8.61929 10.625 10 10.625Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M16.25 8.125C16.25 13.75 10 18.125 10 18.125C10 18.125 3.75 13.75 3.75 8.125C3.75 6.4674 4.40848 4.87769 5.58058 3.70558C6.75269 2.53348 8.3424 1.875 10 1.875C11.6576 1.875 13.2473 2.53348 14.4194 3.70558C15.5915 4.87769 16.25 6.4674 16.25 8.125Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                    <span>VN Office:&nbsp; 155A Thao Nguyen Street, Ecopark township, Van Giang district, Hung Yen province, VN  </span>
+                </div><!--.info-item.address -->
+
+                <div class="info-item contact-email">
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2.5 4.375H17.5V15C17.5 15.1658 17.4342 15.3247 17.3169 15.4419C17.1997 15.5592 17.0408 15.625 16.875 15.625H3.125C2.95924 15.625 2.80027 15.5592 2.68306 15.4419C2.56585 15.3247 2.5 15.1658 2.5 15V4.375Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M17.5 4.375L10 11.25L2.5 4.375" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                    <a href="mailto:cs@64hydro.com">supports@themiracle.com.vn</a>
+                </div><!--.info-item.contact-email -->
+
+                <div class="info-item working-schedule">
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10 17.5C14.1421 17.5 17.5 14.1421 17.5 10C17.5 5.85786 14.1421 2.5 10 2.5C5.85786 2.5 2.5 5.85786 2.5 10C2.5 14.1421 5.85786 17.5 10 17.5Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M10 5.625V10H14.375" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                    <span>Mon - Fri (8:00 - 17:00 - GMT+7)</span>
+                </div><!--.info-item working-schedule-->
+            </div><!--.contact-us-container-->
+
+            <!-- 2. Our company section - 2nd column -->
+            <div class="our-company-container col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <h3 class="block_heading">Our Company</h3>
+                <ul class="list-info-items" >
+                    <li>
+                        <a href="/pages/faqs" class="reversed-link">FAQs</a>
+                    </li>
+                    <li>
+                        <a href="/pages/about-us" class="reversed-link">About Us</a>
+                    </li>
+                </ul>
+            </div><!--.our-company-container-->
+
+            <!-- 3. Customer support section - 3rd column -->
+            <div class="customer-support-container col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3">
+                <h3 class="block_heading">Customer Support</h3>
+                <ul class="list-info-items">
+                    <li>
+                        <a href="/pages/contact-us" class="reversed-link">Contact Us</a>                           
+                    </li>
+                    <li>
+                        <a href="/pages/refund-policy" class="reversed-link">Refund Policy</a>
+                    </li>
+                    <li>
+                        <a href="/pages/shipping-delivery" class="reversed-link">Shipping &amp; Delivery</a>
+                    </li>
+                    <li>
+                        <a href="https://64hydro.com/apps/trackingmore" class="reversed-link">Order Tracking</a>
+                    </li>
+                    <li>
+                        <a href="/policies/privacy-policy" class="reversed-link">Privacy Policy</a>                                   
+                    </li>
+                </ul>
+            </div><!--.customer-support-container -->
+        </div><!--.row.inner-footer-container -->
+        
+    </div><!--#dutapod-footer-container -->
+
+</div><!--.dutapod-footer-outer-container -->
