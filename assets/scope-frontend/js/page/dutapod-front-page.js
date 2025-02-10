25835add3ea0d4c1c9bae07bc94fbe41f35ec99a
sonnhpod <sonnhpod@gmail.com>
2025-02-10 11:10:45 +0700

    Update styling for testinomian section
    Update styling for testinomian section

--- assets/scope-frontend/js/page/dutapod-front-page.js
+++ assets/scope-frontend/js/page/dutapod-front-page.js
@@ -2,18 +2,60 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
+  // Screen sizes variables - in pixel value
+  var minXxlScreenWidth = 1400;
+  var maxXlScreenWidth = 1399;
+  var minXlScreenWidth = 1200;
+  var maxLgScreenWidth = 1199;
+  var minLgScreenWidth = 992;
+  var maxMdScreenWidth = 991;
+  var minMdScreenWidth = 768;
+  var maxSmScreenWidth = 767;
+  var minSmScreenWidth = 576;
+  var maxXsScreenWidth = 575;
   var outermostContentContainerSelector = "div#content.site-content";
   var astraContentContainerSelector = "".concat(outermostContentContainerSelector, " > div.ast-container");
+  var contentAreaContainerSelector = "".concat(astraContentContainerSelector, " > div#primary.content-area");
+  var testimonialSectionID = "sp-testimonial-free-wrapper-999";
   var astraContentContainer = document.querySelector(astraContentContainerSelector);
+  var contentAreaContainer = document.querySelector(contentAreaContainerSelector);
   var astraContentContainerStyleData = window.getComputedStyle(astraContentContainer);
-  var astContentContainerMarginLeft = astraContentContainerStyleData.getPropertyValue('margin-left');
-  var astContentContainerMarginRight = astraContentContainerStyleData.getPropertyValue('margin-right');
-  var astContentContainerPaddingLeft = astraContentContainerStyleData.getPropertyValue('padding-left');
-  var astContentContainerPaddingRight = astraContentContainerStyleData.getPropertyValue('padding-right');
-  console.log("astContentContainerMarginLeft : ".concat(astContentContainerMarginLeft));
-  console.log("astContentContainerMarginRight : ".concat(astContentContainerMarginRight));
-  console.log("astContentContainerPaddingLeft : ".concat(astContentContainerPaddingLeft));
-  console.log("astContentContainerPaddingRight : ".concat(astContentContainerPaddingRight));
+
+  // const astContentContainerMarginLeft = astraContentContainerStyleData.getPropertyValue('margin-left');//OK - 126.667px. Expect 437.22px
+  // const astContentContainerMarginRight = astraContentContainerStyleData.getPropertyValue('margin-right');//OK - 126.667px. Expect 437.22px
+
+  var astContentContainerMarginLeft = astraContentContainerStyleData.marginLeft; //OK. Got 437.22px
+  var astContentContainerMarginRight = astraContentContainerStyleData.marginRight; //OK. Got 437.22px
+
+  astContentContainerMarginLeft = astContentContainerMarginLeft.substring(0, astContentContainerMarginLeft.length - 2); //437.22 in string format
+
+  // const astContentContainerPaddingLeft = astraContentContainerStyleData.getPropertyValue('padding-left');//OK - 20px
+  // const astContentContainerPaddingRight = astraContentContainerStyleData.getPropertyValue('padding-right');//OK - 20px
+
+  var astContentContainerPaddingLeft = astraContentContainerStyleData.paddingLeft;
+  astContentContainerPaddingLeft = astContentContainerPaddingLeft.substring(0, astContentContainerPaddingLeft.length - 2); // 20 in string format
+
+  var astContentContainerPaddingRight = astraContentContainerStyleData.paddingRight;
+
+  /** 1. Add negative margin value to widen the size of the testimonial section - with the screen size >= md width */
+  if (window.screen.width > minSmScreenWidth) {
+    var testimonialSection = document.getElementById(testimonialSectionID);
+    var testimonialSectionWidth = testimonialSection.offsetWidth; // get testimonial Section 90vw width in pixel    
+
+    var testimonialSectionScreenMarginLeft = 0.05 * window.screen.width; // 5vw - 5% view port width
+
+    var testimonialSectionMarginLeft = parseFloat(astContentContainerMarginLeft) + parseFloat(astContentContainerPaddingLeft) - parseFloat(testimonialSectionScreenMarginLeft);
+    testimonialSectionMarginLeft *= -1;
+
+    // Set negative margin left value 
+    // testimonialSection.style.marginLeft = `-${testimonialSectionMarginLeft}px`;
+    testimonialSection.style.marginLeft = "".concat(testimonialSectionMarginLeft, "px");
+  }
+
+  // console.log(`astContentContainerMarginLeft : ${astContentContainerMarginLeft}`);
+  // console.log(`astContentContainerMarginRight : ${astContentContainerMarginRight}`);
+  // console.log(`astContentContainerPaddingLeft : ${astContentContainerPaddingLeft}`);
+  // console.log(`astContentContainerPaddingRight : ${astContentContainerPaddingRight}`);
 });
 
 },{}]},{},[1]);
--- sources/scope-frontend/js/page/dutapod-front-page.js
+++ sources/scope-frontend/js/page/dutapod-front-page.js
@@ -1,13 +1,64 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener( 'DOMContentLoaded', function(){
+    // Screen sizes variables - in pixel value
+    const minXxlScreenWidth = 1400;
+
+    const maxXlScreenWidth = 1399;
+    const minXlScreenWidth = 1200;
+
+    const maxLgScreenWidth = 1199;
+    const minLgScreenWidth = 992;
+    
+    const maxMdScreenWidth = 991;
+    const minMdScreenWidth = 768;
+    
+    const maxSmScreenWidth = 767;
+    const minSmScreenWidth = 576;
+
+    const maxXsScreenWidth = 575;
+
     const outermostContentContainerSelector = `div#content.site-content`;
     const astraContentContainerSelector = `${outermostContentContainerSelector} > div.ast-container`;
+    const contentAreaContainerSelector = `${astraContentContainerSelector} > div#primary.content-area`;
+
+    const testimonialSectionID = `sp-testimonial-free-wrapper-999`;
+    
+
     const astraContentContainer = document.querySelector( astraContentContainerSelector );
+    const contentAreaContainer = document.querySelector( contentAreaContainerSelector );
 
     const astraContentContainerStyleData = window.getComputedStyle( astraContentContainer );
-    const astContentContainerMarginLeft = astraContentContainerStyleData.getPropertyValue('margin-left');//OK
-    const astContentContainerMarginRight = astraContentContainerStyleData.getPropertyValue('margin-right');//OK
-    const astContentContainerPaddingLeft = astraContentContainerStyleData.getPropertyValue('padding-left');//OK
-    const astContentContainerPaddingRight = astraContentContainerStyleData.getPropertyValue('padding-right');//OK
+    
+    // const astContentContainerMarginLeft = astraContentContainerStyleData.getPropertyValue('margin-left');//OK - 126.667px. Expect 437.22px
+    // const astContentContainerMarginRight = astraContentContainerStyleData.getPropertyValue('margin-right');//OK - 126.667px. Expect 437.22px
+    
+    let astContentContainerMarginLeft = astraContentContainerStyleData.marginLeft;//OK. Got 437.22px
+    let astContentContainerMarginRight = astraContentContainerStyleData.marginRight;//OK. Got 437.22px
+
+    astContentContainerMarginLeft = astContentContainerMarginLeft.substring(0, astContentContainerMarginLeft.length - 2 );//437.22 in string format
+
+    // const astContentContainerPaddingLeft = astraContentContainerStyleData.getPropertyValue('padding-left');//OK - 20px
+    // const astContentContainerPaddingRight = astraContentContainerStyleData.getPropertyValue('padding-right');//OK - 20px
+
+    let astContentContainerPaddingLeft = astraContentContainerStyleData.paddingLeft;
+    astContentContainerPaddingLeft = astContentContainerPaddingLeft.substring(0, astContentContainerPaddingLeft.length - 2 );// 20 in string format
+
+    let astContentContainerPaddingRight = astraContentContainerStyleData.paddingRight;
+
+     /** 1. Add negative margin value to widen the size of the testimonial section - with the screen size >= md width */
+    if( window.screen.width > minMdScreenWidth ){       
+        const testimonialSection = document.getElementById( testimonialSectionID );
+
+        let testimonialSectionWidth = testimonialSection.offsetWidth; // get testimonial Section 90vw width in pixel    
+
+        let testimonialSectionScreenMarginLeft = 0.05 * window.screen.width; // 5vw - 5% view port width
+
+        let testimonialSectionMarginLeft = parseFloat(astContentContainerMarginLeft) + parseFloat(astContentContainerPaddingLeft) - parseFloat( testimonialSectionScreenMarginLeft );
+        testimonialSectionMarginLeft *= -1; 
+        
+        // Set negative margin left value 
+        // testimonialSection.style.marginLeft = `-${testimonialSectionMarginLeft}px`;
+        testimonialSection.style.marginLeft = `${testimonialSectionMarginLeft}px`;
+    }    
 
     // console.log(`astContentContainerMarginLeft : ${astContentContainerMarginLeft}`);
     // console.log(`astContentContainerMarginRight : ${astContentContainerMarginRight}`);
