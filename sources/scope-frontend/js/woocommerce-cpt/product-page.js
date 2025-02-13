bebd69c5857b8728d4cf2930009b3bad6af7989f
sonnhpod <sonnhpod@gmail.com>
2025-02-13 16:56:13 +0700

    Update product page display - product description accordion
    Update product page display - product description accordion

--- sources/scope-frontend/js/woocommerce-cpt/product-page.js
+++ sources/scope-frontend/js/woocommerce-cpt/product-page.js
@@ -6,13 +6,38 @@ document.addEventListener('DOMContentLoaded', function(){
         e.preventDefault();
         if( productDesContentContainer.classList.contains('open') ){
             // Close the accordion description
-            productDesContentContainer.style.maxHeight = null;                   
+            // productDesContentContainer.style.maxHeight = null;      
+            productDesContentContainer.style.maxHeight = '0';                
         } else{
             // Open the accordion description
-            productDesContentContainer.style.maxHeight = `${productDesContentContainer.scrollHeight}px`;      
+            // let height = getFullHeightDOMElement( productDesContentContainer );
+            let productDesContentContainerHeight = parseFloat(productDesContentContainer.scrollHeight) + 30;// Add 20 pixel more
+            // productDesContentContainer.style.height = `${height}px`;
+            productDesContentContainer.style.maxHeight = `${productDesContentContainerHeight}px`;    
+            // productDesContentContainer.style.maxHeight = '100%';
+            // productDesContentContainer.style.height = '100%';  
         }
 
         productDesContentContainer.classList.toggle('open');        
         productDesToggleBtn.classList.toggle('open');
     });
 });
+
+function getFullHeightDOMElement(element) {
+    // Temporarily make the element visible if it's hidden
+    element.style.visibility = "hidden";
+    element.style.position = "absolute";
+    element.style.display = "block";
+
+    // Get the full height
+    let fullHeight = element.scrollHeight;
+
+    // Revert styles back
+    element.style.removeProperty("visibility");
+    element.style.removeProperty("position");
+    element.style.display = "none";
+
+    return fullHeight;
+}
+
+
