0d11b98a53e69e549dd8d4e8bfc26d076486a96f
sonnhpod <sonnhpod@gmail.com>
2025-02-26 14:13:14 +0700

    Update gulp task for compiling admin setting pages
    Update gulp task for compiling admin setting pages

--- gulpfile.js
+++ gulpfile.js
@@ -67,17 +67,17 @@ const resourcesInfo = {
         prelib:{
             srcBootstrapFile:'node_modules/bootstrap/scss/bootstrap.scss',
             srcDir:'./sources/scope-prelib/scss/',
-            srcListFile:'./sources/scope-prelib/scss/*.scss',
+            srcListFile:'./sources/scope-prelib/scss/**/*.scss',
             distDir:'./assets/scope-prelib/css/'
         },
         admin:{
-            scrDir:'./sources/scope-admin/scss/',
-            scrListFile:'./sources/scope-admin/scss/*.scss',
+            srcDir:'./sources/scope-admin/scss/',
+            srcListFile:'./sources/scope-admin/scss/**/*.scss',
             distDir:'./assets/scope-admin/css/',
         },
         editor:{
             srcDir:'./sources/scope-editor/scss/',
-            scrListFile:'./sources/scope-editor/scss/*.scss',
+            srcListFile:'./sources/scope-editor/scss/**/*.scss',
             distDir:'./assets/scope-editor/css/',
         },
         frontend:{
@@ -115,13 +115,7 @@ const resourcesInfo = {
 /** All elemental tasks must be defined before declaring composite gulp tasks */
 /**============================================================= **/
 
-
-/**=== 3.1. Distribute all styles files - SCSS to CSS === **/
-
-gulp.task( 'distribute-all-frontend-styles', distribute_all_frontend_styles );
-
-/**=== 3.1.1. Distribute all prelib styles files for frontend display === **/
-
+/** 3.1. Distribute all prelib styles and scripts */
 gulp.task('distribute-prelib-tailwindcss-styles', function( done ){
     
     let srcStyleFile = './sources/scope-prelib/scss/tailwindcss-full.scss';
@@ -151,7 +145,10 @@ function distribute_prelib_tailwind_styles(srcStyleFile, distStyleDir){
         .pipe( browserSync.stream() );
 }
 
-/**=== 3.1.2. Distribute all styles files for frontend display === **/
+/** 3.2. Distribute all styles and scripts for frontend display */
+/** 3.2.1. Distribute all styles files - SCSS to CSS === **/
+
+gulp.task( 'distribute-all-frontend-styles', distribute_all_frontend_styles );
 
 function distribute_all_frontend_styles( done ){
     /** 1. Distribute all frontend display SCSS to CSS in beauty format */
@@ -164,91 +161,7 @@ function distribute_all_frontend_styles( done ){
 };//distribute_all_frontend_styles
 
 
-/**=== 3.1-extra Helper functions Distribute all styles files - SCSS to CSS === **/
-/** 1. Distribute all SCSS to CSS */
-function distribute_all_scss_to_css_beauty_format( scssSourceDir, cssDistDir){
-    /**
-     * Temporary remove the ".pipe( gulpSourceMaps.write('./') )" before gulp.dest
-    */
-    return gulp.src( scssSourceDir )
-        .pipe( gulpPlumber() )
-        .pipe( gulpSourceMaps.init() )
-        .pipe(
-            gulpSass(
-                {
-                    includePath:['node_modules/bootstrap/scss'],
-                    errorLogToConsole: true
-                }
-            )
-            .on( 'error', console.error.bind( console ) )
-        )
-        .pipe(
-            gulpAutoPrefixer( { cascade: false } )
-        )
-        .pipe( gulp.dest( cssDistDir ) )
-        .pipe( browserSync.stream() );
-
-}//distribute_all_scss_to_css_beauty_format
-
-/** 2. Distribute single file SCSS to CSS */
-function distribute_single_scss_to_css_beauty_format( scssSrcFilePath, cssDistDir ){
-    /**
-     * Temporary remove the ".pipe( gulpSourceMaps.write('./') )" before gulp.dest
-    */
-    return gulp.src( scssSrcFilePath )
-        .pipe( gulpPlumber() )
-        .pipe( gulpSourceMaps.init() )
-        .pipe(
-            gulpSass(
-                {
-                    includePath:['node_modules/bootstrap/scss'],
-                    errorLogToConsole: true
-                }
-            )
-            .on( 'error', console.error.bind( console ) )
-        )
-        .pipe( 
-            gulpRenamer( 
-                function(filePath){ 
-                    filePath.basename += "-import"; 
-                    filePath.extname = ".css";
-                } 
-            ) 
-        )
-        .pipe( gulp.dest( cssDistDir ) )
-        .pipe( browserSync.stream() );
-
-}//distribute_single_scss_to_css_beauty_format
-
-/** 3. Distribute all SCSS to minified CSS */
-function distribute_all_scss_to_css_minified_format( scssSourceDir, cssDistDir ){
-    /**
-     * Temporary remove the ".pipe( gulpSourceMaps.write('./') )" before gulp.dest
-    */
-    return gulp.src( scssSourceDir )
-        .pipe( gulpPlumber() )
-        .pipe( gulpSourceMaps.init() )
-        .pipe(
-            gulpSass(
-                {
-                    includePath:['node_modules/bootstrap/scss'],
-                    errorLogToConsole: true,
-                    outputStyle:'compressed'
-                }
-            )
-            .on( 'error', console.error.bind( console ) )
-        )
-        .pipe(
-            gulpAutoPrefixer( { cascasde: false } )
-        )
-        .pipe( gulpRenamer( { suffix: '.min' } ) )
-        .pipe( gulp.dest( cssDistDir ) )
-        .pipe( browserSync.stream() );
-}//distribute_all_scss_to_css_minified_format
-
-
-
-/**=== 3.2. Distribute all script files - JS ES6 to Vanilla JS === **/
+/**=== 3.2.2. Distribute all script files - JS ES6 to Vanilla JS === **/
 
 gulp.task( 'distribute-all-frontend-scripts', distribute_all_frontend_scripts_recursively_2 );
 /** 3.2.1. Distribute all frontend JS ES 6 to vanilla JS */
@@ -455,8 +368,182 @@ function distribute_all_frontend_scripts_recursively_3( done ){
 
 
 
-/**=== 3.2-extra Helper functions Distribute all scripts files - JS ES6 to Vanilla JS === **/
-/** 1. Distribute a single modern JS file to vanilla JS file */
+
+
+/***************************/
+/** 3.2. Distribute admin styles and scripts */
+/** 3.2.1. Distribute admin style */
+gulp.task( 'distribute-all-admin-styles', distribute_all_admin_styles );
+
+function distribute_all_admin_styles( done ){
+    /** 1. Distribute all frontend display SCSS to CSS in beauty format */
+    distribute_all_scss_to_css_beauty_format(
+        resourcesInfo.styles.admin.srcListFile,
+        resourcesInfo.styles.admin.distDir
+    );
+
+    done();
+};//distribute_all_frontend_styles
+
+/** 3.2.2. Distribute admin script */
+
+gulp.task( 'distribute-all-admin-scripts', distribute_all_admin_scripts_recursively );
+
+function distribute_all_admin_scripts_recursively( done ){
+
+    const getAllSubDirectories = async function( srcDir ){
+        let subDirList = [];
+        let dirItems = await fsPromise.readdir( srcDir, {withFileTypes: true} );
+
+        for( let dirItem of dirItems ){
+            if( dirItem.isDirectory() ){
+                let fullPathDirectory = path.join( srcDir, dirItem.name );
+                subDirList.push( fullPathDirectory );
+                // Recursively get sub directories inside this directory
+                const nestedSubDirectories = await getAllSubDirectories( fullPathDirectory );
+                subDirList = subDirList.concat( nestedSubDirectories );
+            }
+        }
+
+        return subDirList;
+        // return dirItems.filter( dirItem => dirItem.isDirectory() ).map( dirItem => dirItem.name );
+    }     
+
+    /** 1. Distribute all JS files in the outer directory */
+    distribute_all_modern_js_to_vanilla_js(
+        resourcesInfo.scripts.admin.srcDir,
+        resourcesInfo.scripts.admin.srcListFile,
+        resourcesInfo.scripts.admin.distDir
+    );
+
+    /** 2. Iterate through each sub directory, and distribute all JS files */
+    getAllSubDirectories( resourcesInfo.scripts.admin.srcDir )
+        .then( subDirectories => ( 
+            function( subDirectories ){ 
+                    const subDirList = Object.values( subDirectories );// OK
+
+                    // console.log( subDirList );
+                    for( let [index, subDir] of Object.entries( subDirectories ) ){
+                        let srcDir = subDir;//OK
+                        let srcListFile = path.join( subDir, '*.js' );//OK
+                        // console.log(`Current srcListFile : ${srcListFile}`);
+                        let distDir = srcDir.replace( /^sources[\/\\]/, 'assets\\' );// OK with hardcode
+                        // let distDir = srcDir.replace( `/^sources[\/\${path.sep}]/`, `assets\${path.sep}` );
+                        // Append the prefix to indicate the relative directory
+                        srcDir = `.\\${srcDir}\\`;
+                        srcListFile = `.\\${srcListFile}`;
+                        distDir = `.\\${distDir}\\`;
+
+                        // Replate the default path separator (path.sep) with the path separator that gulp task can intepret: 
+                        srcDir = srcDir.replaceAll('\\','\/');
+                        srcListFile = srcListFile.replaceAll('\\','\/');
+                        distDir = distDir.replaceAll('\\','\/');
+
+                        distribute_all_modern_js_to_vanilla_js( srcDir, srcListFile, distDir );
+                    }
+            
+                }
+            )( subDirectories ) 
+        )
+        .catch( err => console.error( err ) );
+
+    /*
+    getAllSubDirectories( resourcesInfo.scripts.frontend.srcDir )
+        .then( subDirectory => console.log( subDirectory ) )
+        .catch( err => console.error( err ) );
+    */
+
+    done();
+}//distribute_all_admin_scripts_recursively
+
+/*****************************************************************/
+/** 4. Helper functions for (3) category ***/
+
+/** 4.1. Distribute all SCSS to CSS */
+/** 4.1.1. Helper functions Distribute all styles files - SCSS to CSS **/
+function distribute_all_scss_to_css_beauty_format( scssSourceDir, cssDistDir){
+    /**
+     * Temporary remove the ".pipe( gulpSourceMaps.write('./') )" before gulp.dest
+    */
+    return gulp.src( scssSourceDir )
+        .pipe( gulpPlumber() )
+        .pipe( gulpSourceMaps.init() )
+        .pipe(
+            gulpSass(
+                {
+                    includePath:['node_modules/bootstrap/scss'],
+                    errorLogToConsole: true
+                }
+            )
+            .on( 'error', console.error.bind( console ) )
+        )
+        .pipe(
+            gulpAutoPrefixer( { cascade: false } )
+        )
+        .pipe( gulp.dest( cssDistDir ) )
+        .pipe( browserSync.stream() );
+
+}//distribute_all_scss_to_css_beauty_format
+
+/** 4.1.2. Distribute single file SCSS to CSS */
+function distribute_single_scss_to_css_beauty_format( scssSrcFilePath, cssDistDir ){
+    /**
+     * Temporary remove the ".pipe( gulpSourceMaps.write('./') )" before gulp.dest
+    */
+    return gulp.src( scssSrcFilePath )
+        .pipe( gulpPlumber() )
+        .pipe( gulpSourceMaps.init() )
+        .pipe(
+            gulpSass(
+                {
+                    includePath:['node_modules/bootstrap/scss'],
+                    errorLogToConsole: true
+                }
+            )
+            .on( 'error', console.error.bind( console ) )
+        )
+        .pipe( 
+            gulpRenamer( 
+                function(filePath){ 
+                    filePath.basename += "-import"; 
+                    filePath.extname = ".css";
+                } 
+            ) 
+        )
+        .pipe( gulp.dest( cssDistDir ) )
+        .pipe( browserSync.stream() );
+
+}//distribute_single_scss_to_css_beauty_format
+
+/** 4.1.3. Distribute all SCSS to minified CSS */
+function distribute_all_scss_to_css_minified_format( scssSourceDir, cssDistDir ){
+    /**
+     * Temporary remove the ".pipe( gulpSourceMaps.write('./') )" before gulp.dest
+    */
+    return gulp.src( scssSourceDir )
+        .pipe( gulpPlumber() )
+        .pipe( gulpSourceMaps.init() )
+        .pipe(
+            gulpSass(
+                {
+                    includePath:['node_modules/bootstrap/scss'],
+                    errorLogToConsole: true,
+                    outputStyle:'compressed'
+                }
+            )
+            .on( 'error', console.error.bind( console ) )
+        )
+        .pipe(
+            gulpAutoPrefixer( { cascasde: false } )
+        )
+        .pipe( gulpRenamer( { suffix: '.min' } ) )
+        .pipe( gulp.dest( cssDistDir ) )
+        .pipe( browserSync.stream() );
+}//distribute_all_scss_to_css_minified_format
+
+/** 4.2. Distribute JS scripts to traditional JS */
+/** Helper functions Distribute all scripts files - JS ES6 to Vanilla JS === **/
+/** 4.2.1. Distribute a single modern JS file to vanilla JS file */
 function distribute_single_esnext_js_to_vanilla_js( jsSrcFileDir, jsSrcFileName, jsDistDir ){
     /**
      * Temporary remove the ".pipe( gulpSourceMaps.write('./') )" before gulp.dest
@@ -477,7 +564,7 @@ function distribute_single_esnext_js_to_vanilla_js( jsSrcFileDir, jsSrcFileName,
         .pipe( browserSync.stream() );
 }//distribute_single_modern_js_to_vanilla_js
 
-/** 2. Distribute all modern JS files to vanilla JS */
+/** 4.2.2. Distribute all modern JS files to vanilla JS */
 function distribute_all_modern_js_to_vanilla_js( jsSrcFileDir, srcListFile, jsDistDir ){
     // the srcListFile must have glob format, such as: ""./sources/scope-frontend/js/*.js"
     
@@ -496,7 +583,8 @@ function distribute_all_modern_js_to_vanilla_js( jsSrcFileDir, srcListFile, jsDi
 }//distribute_all_modern_js_to_vanilla_js
 
 
-/**=== 4, Global tasks === */
+/***********************************************************************/
+/**=== 5. Global tasks === */
 
 /** 4.1. Distribute all prelib resources */
 gulp.task(
@@ -506,8 +594,18 @@ gulp.task(
     )
 );
 
-/** 4.2. Distribute all frontend resources **/
 
+/** 4.2. Distribute all admin resources **/
+gulp.task(
+    'distribute-all-admin-resources',
+    gulp.series( 
+        'distribute-all-admin-styles', 
+        'distribute-all-admin-scripts'
+    )
+);
+
+
+/** 4.3. Distribute all frontend resources **/
 
 /*
 gulp.task(
@@ -531,9 +629,7 @@ gulp.task(
     )
 );
 
-/** 4.2. Distribute all admin resources **/
-
-/** 4.3. Distribute all editor resources **/
+/** 4.4. Distribute all editor resources **/
 
 /**============================================================= **/
 /**=== 5. Troubleshotting === **/
